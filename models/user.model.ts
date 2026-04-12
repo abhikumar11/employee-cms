79a9d2e328323eaa36e652a4679a4ae756328bc0
abhikumar11 <abhishekkumar12verma@gmail.com>
2026-04-12 08:45:46 +0530

    created schema and connection to db

---
+++ models/user.model.ts
@@ -0,0 +1,13 @@
+import { Employee } from "@/types/employe.types";
+import mongoose from "mongoose";
+
+const employeeSchema = new mongoose.Schema<Employee>({
+    id: { type: Number, required: true, unique: true },
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    department: { type: String, required: true },
+    joiningDate: { type: Date, default: Date.now }
+});
+const EmployeeModel = mongoose.model<Employee>("Employee", employeeSchema);
+export default EmployeeModel;
